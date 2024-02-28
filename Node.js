620c8b4b82480f3d94b640bd198dbd60aa530ce4
Muna-Zeer <munaelzeer@gmail.com>
2024-02-28 19:57:42 +0200

    node-js

---
+++ Node.js
@@ -0,0 +1,10 @@
+class Node{
+    constructor(data){
+        //to store the value of data
+        this.data=data;
+        //To reference prev node
+        this.prev=null;
+        //to reference next node
+         this.next=null;
+    }
+}
