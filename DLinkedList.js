8f4df1b96f692821db7a2a16c4b827ecde6a53cf
Muna-Zeer <munaelzeer@gmail.com>
2024-03-01 15:03:17 +0200

    remove-insert-methods

--- DLinkedList.js
+++ DLinkedList.js
@@ -1,146 +1,146 @@
-import Node from './Node.js'
-class DoublyLinkedList{
-    constructor(){
-        // the first node in Double LinkedList
-        this.head=null;
-        //the last node in Double LinkedList;
-        this.tail=null;
-        this.length=0;
+import Node from "./Node.js";
+class DoublyLinkedList {
+  constructor() {
+    // the first node in Double LinkedList
+    this.head = null;
+    //the last node in Double LinkedList;
+    this.tail = null;
+    this.length = 0;
+  }
+  // important operations in DoublyLinkedList
+  isEmpty() {
+    return (this.length = 0);
+  }
+
+  //append newNode to the end of the list
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
     }
-    // important operations in DoublyLinkedList
-    isEmpty(){
-        if(this.head=null)return true
-        return false;
+    this.length++; 
+}
+  //remove last node form the list and return the lst node
+
+  pop() {
+    if (!this.tail) return null; // Check if list is empty
+    let lastNode = this.tail;
+    this.tail = lastNode.prev;
+    if (this.tail) {
+        this.tail.next = null;
+    } else {
+        this.head = null;
+    }
+    lastNode.prev = null;
+    this.length--;
+    return lastNode.data;
+}
+  //delete first node from the list
+
+shift() {
+    if (!this.head) return null; 
+    let firstNode = this.head;
+    this.head = firstNode.next;
+    if (this.head) {
+        this.head.prev = null;
+    } else {
+        this.tail = null;
+    }
+    firstNode.next = null;
+    this.length--;
+    return firstNode.data
+    ;
+}
+
+
+  //add Node in the head of the list
+  unshift(val) {
+    let headNode = new Node(val);
+    if (!this.head) {
+      this.head = headNode;
+      this.tail = headNode;
+    } else {
+      this.head.prev = headNode;
+      headNode.next = this.head;
+      this.head = headNode;
+    }
+    this.length++;
+    return headNode;
+  }
+  // The get method takes an index and return the value of node
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return null;
     }
 
-    //append newNode to the end of the list 
-    push(val){
-    
-    let newNode= new Node(val);
-    if(!this.head){
-       this.head=newNode;
-       this.tail=newNode;
+    let curNode = this.head;
+    for (let i = 0; i < index; i++) {
+      if (!curNode) return null;
+      curNode = curNode.next;
     }
-    else{
-        this.tail.next=newNode;
-        this.newNode.prev=tail;
-        this.tail=newNode;
+    return curNode ? curNode.data : null;
+  }
+
+  //inset newNode based on it's index
+  set(index, value) {
+    let curNode=this.head;
+    for(let i=0;i<index&&curNode!==null;i++){
+        curNode=curNode.next;
     }
-}
- //remove last node form the list and return the lst node
-pop(){
-if(!this.tail){
-   
-return null;
-}
-let lastNode=this.tail;
-this.tail=lastNode.prev;
-this.tail.next=null;
-lastNode.prev=null;
-
-return lastNode;
-}
-//delete first node from the list
-shift(){
-    if(!this.head){return null;}
-    let firstNode=this.head;
-    this.head.prev=null;
-    firstNode.next=null;
-return firstNode;
-}
-
-//add Node in the head of the list 
-unshift(val){
-let headNode=new Node(val);
-if(!this.head){
-    this.head=headNode;
-    this.tail=headNode;
-}
-else{
-    this.head.prev=headNode;
-headNode.next=this.head;
-this.head=headNode;
-
-}
-return headNode;
-}
-// The get method takes an index number as a parameter and returns the value of the node at that index
-get(index){
-if(index<0){return null;}
-
-let curNode;
-//if index value is closer from the first part of the list
-if(index< this.length/2){
-curNode=this.head;
-for(let i=0;i<index&&curNode!=null;i++){
-    curNode=curNode.next;
-}
-}
-else{
-    //if the index of value is closer from the end of the list
-    curNode=this.tail;
-    for(let i=this.length-1;i>index&&curNode!=null;i--){
-        curNode=curNode.prev;
-    }
-}
-
-return curNode ?curNode.val:null;
-
-}
-
-//inset newNode based on it's index
-set(index,value){
-    let determinedNode=this.get(index);
-    if(determinedNode!=null){
-        determinedNode.value=value;
-return true;
+    if(curNode!==null){
+        curNode.data=value;
+        return true;
     }
     return false;
+  }
+
+  //inset newNode based on it's index
+  insert(index, val) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === 0) {
+      return !!this.unshift(val);
+    }
+    if (index == this.length) return !!this.push(val);
+
+    let newNode = new Node(val);
+    let curNode=this.head;
+for(let i=0;i<index&&curNode!==null;i++){
+    curNode=curNode.next;
+
 }
+newNode.next=curNode.next;
+newNode.prev=curNode;
+curNode.next.prev=newNode;
+curNode.next=newNode;
+    this.length++;
+    return true;
+  }
 
-   //inset newNode based on it's index
-   insert(index, val) {
-if(index<0||index>this.length){return false;}
-if(index===0){
-    return this.unshift(val);
-}
-if(index==this.length)return this.push(val);
-    
-let newNode=new Node(val);
-let prevNode=this.get(index-1);
-let nextNode=prevNode.next;
+  //remove Node based on its index
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
 
-prevNode.next=newNode;
-newNode.prev=prevNode;
-newNode.next=nextNode;
-nextNode.prev=newNode;
-
-this.length++;
-return true;
-   }
-
-   //remove Node based on its index
-   remove(index){
-    if(index<0||index>this.length){return undefined}
-    if(index===this.length-1) return this.pop()
-   
-if(index==0)return this.shift();
-
-let deletedNode=this.get(index);
-
-let prevNode=deletedNode.prev;
-let nextNode=deletedNode.next;
-
-prevNode.nextNode=nextNode;
-nextNode.prev=prevNode;
-
-
-deletedNode.next=null;
-deletedNode.prev=null;
-this.length--;
-return true;
+    let curNode = this.head;
+    for (let i = 0; i < index; i++) {
+        curNode = curNode.next;
+    }
+    let removedNode = curNode;
+    curNode.prev.next = curNode.next;
+    curNode.next.prev = curNode.prev;
+    removedNode.prev = null;
+    removedNode.next = null;
+    this.length--;
+    return removedNode.data;
 }
 }
-
 
 export default DoublyLinkedList;
--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 
-import DLinkedList from './DLinkedList.js';
-const dll=new DoublyLinkedList();
+import DoublyLinkedList from './DLinkedList.js';
 
-dll.push(4);
-dll.push(5);
-dll.push(6);
-console.log("display all Items in the list",dll);
+// Create a new instance of DoublyLinkedList
+const dll = new DoublyLinkedList();
+
+
+dll.push(10); // Add 10 to the end of the list
+dll.push(20); 
+dll.push(30); 
+dll.push(40); 
+dll.push(50); 
+
+
+console.log("display current linked list :");
+console.log(dll); 
+dll.unshift(5); // Add 5 to the beginning of the list
+console.log("display current linked list by unshift :");
+console.log(dll); 
+console.log("remove the last node form the list :", dll.pop()); //remove the last node form the list 
+console.log("remove the first node form the list :", dll.shift()); // remove the first node from the list 
+
+console.log("display dll after removed items :");
+console.log(dll); 
+
+console.log("get access the item of position 1:", dll.get(1)); // get access the item of position 1
+
+console.log("Set node at index 1 to 2024:", dll.set(1, 2024)); // Set the item at index 1 to 50
+
+dll.insert(1, 30); // insert 30 at index 2
+
+console.log("display dll after insertion:");
+console.log(dll); // display current dll after insertion
+
+console.log("remove node based in specific position 1:", dll.remove(2)); // remove node based in specific position
+
+console.log(" after all modification look to final dll:");
+console.log(dll); 
